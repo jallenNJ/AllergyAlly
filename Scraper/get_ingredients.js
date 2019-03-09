132fb7275d84ee46e192ed22d99f5f9f7a8758e5
Brendan <brendan991@gmail.com>
2019-03-09 14:43:18 -0500

    get search url for each item

--- Scraper/get_ingredients.js
+++ Scraper/get_ingredients.js
@@ -1,6 +1,6 @@
 const scrape = require('website-scraper');
-const url = "http://foodnetwork.com/search/";
-
+const base_url = "http://foodnetwork.com/search/";
+let food_items_urls = [];
 
 let food_items = ["pizza", "burger", "sandwich"];
 
@@ -10,11 +10,22 @@ function get_ingredients(food_items)
 {
 	for (i = 0; i < food_items.length; i++)
 	{
+		food_items_urls.push(base_url.concat(food_items[i]));
 		console.log(food_items[i]);
+		console.log(food_items_urls);
 	}
 
+	scrape({
+		urls: food_items_urls,
+		directory: './foodname',
+		sources: [
+			{selector: 'div', attr: 'src'}
+		]
+	});
 
-	//let map = {name :
+
+
+
 }
 /*
 scrape({
