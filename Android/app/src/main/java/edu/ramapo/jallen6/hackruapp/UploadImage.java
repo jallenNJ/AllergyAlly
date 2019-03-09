3b4c87c441a7300fd300dfab94354e6d56f5d4ef
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-03-09 15:58:54 -0500

    Ui for selected image and seperate buttons for select / upload

--- Android/app/src/main/java/edu/ramapo/jallen6/hackruapp/UploadImage.java
+++ Android/app/src/main/java/edu/ramapo/jallen6/hackruapp/UploadImage.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 import com.android.volley.Request;
@@ -27,15 +28,16 @@ public class UploadImage extends AppCompatActivity {
 
     private final static int LOAD_IMG = 1;
 
+    private Bitmap selectedImage;
+    ImageView selectedPhoto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_image);
 
+        selectedPhoto = findViewById(R.id.uploadImageSelectedImage);
 
-        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-        photoPickerIntent.setType("image/*");
-        startActivityForResult(photoPickerIntent, LOAD_IMG);
     }
 
 
@@ -47,41 +49,18 @@ public class UploadImage extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             try {
                 final Uri imageUri = data.getData();
-                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                ImageView image_view = findViewById(R.id.uploadImageSelectedImage);
-                image_view.setImageBitmap(selectedImage);
-
-                String base64Image = getStringImage(selectedImage);
-
-                Log.i("ImageString", base64Image);
-                JSONObject params = new JSONObject(); //Add allergies as a string array
-                try {
-                    params.put("menu", base64Image);
-                } catch (JSONException e) {
-                    Toast.makeText(UploadImage.this, "Failed to put", Toast.LENGTH_SHORT).show();
-                    e.printStackTrace();
+                if(imageUri == null){
+                    Log.e("NullCheck", "imageURI (Selected image) was null");
                     return;
                 }
-                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
-                        "http://192.168.17.118:3000/Allergens",
-                        params,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                Toast.makeText(UploadImage.this, "Boom answer", Toast.LENGTH_SHORT).show();
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(UploadImage.this, "Error", Toast.LENGTH_SHORT).show();
-                                error.printStackTrace();
-                            }
-                        }
-                );
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                selectedImage = BitmapFactory.decodeStream(imageStream);
 
-                Volley.newRequestQueue(getApplicationContext()).add(request);
+                selectedPhoto.setImageBitmap(selectedImage);
+
+                findViewById(R.id.uploadImage_ChooseButton).setVisibility(View.GONE);
+                findViewById(R.id.uploadImage_UploadButton).setVisibility(View.VISIBLE);
+
 
 
 
@@ -101,6 +80,47 @@ public class UploadImage extends AppCompatActivity {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
         return Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+
+    }
+
+
+    public void chooseImage(View v){
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, LOAD_IMG);
+    }
+
+    public void uploadImage(View v){
+        String base64Image = getStringImage(selectedImage);
+
+        Log.i("ImageString", base64Image);
+        JSONObject params = new JSONObject(); //Add allergies as a string array
+        try {
+            params.put("menu", base64Image);
+        } catch (JSONException e) {
+            Toast.makeText(UploadImage.this, "Failed to put", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+            return;
+        }
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
+                "http://192.168.17.118:3000/Allergen",
+                params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(UploadImage.this, "Boom answer", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(UploadImage.this, "Error", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                }
+        );
+
+        Volley.newRequestQueue(getApplicationContext()).add(request);
 
     }
 }
