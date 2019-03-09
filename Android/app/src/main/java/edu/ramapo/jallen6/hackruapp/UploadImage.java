ecdce2b48c433868d07e070bd9d7998f5fae6092
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-03-09 14:50:20 -0500

    Able to parse image as a Base 64 string

--- Android/app/src/main/java/edu/ramapo/jallen6/hackruapp/UploadImage.java
+++ Android/app/src/main/java/edu/ramapo/jallen6/hackruapp/UploadImage.java
@@ -4,11 +4,21 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
@@ -38,8 +48,40 @@ public class UploadImage extends AppCompatActivity {
                 final Uri imageUri = data.getData();
                 final InputStream imageStream = getContentResolver().openInputStream(imageUri);
                 final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                ImageView image_view = new ImageView(this);
+                ImageView image_view = findViewById(R.id.uploadImageSelectedImage);
                 image_view.setImageBitmap(selectedImage);
+
+                String base64Image = getStringImage(selectedImage);
+
+                Log.i("ImageString", base64Image);
+                JSONObject params = new JSONObject(); //Add allergies as a string array
+                try {
+                    params.put("menu", base64Image);
+                } catch (JSONException e) {
+                    Toast.makeText(UploadImage.this, "Failed to put", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                    return;
+                }
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
+                        "http://192.168.17.118/Allergens",
+                        params,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(UploadImage.this, "Boom answer", Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(UploadImage.this, "Error", Toast.LENGTH_SHORT).show();
+                                error.printStackTrace();
+                            }
+                        }
+                );
+
+
+
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
                 Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
@@ -48,5 +90,14 @@ public class UploadImage extends AppCompatActivity {
         }else {
             Toast.makeText(this, "You haven't picked Image",Toast.LENGTH_LONG).show();
         }
+    }
+
+
+
+    public String getStringImage(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        return Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+
     }
 }
