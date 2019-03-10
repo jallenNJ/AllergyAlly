fef59957709db52e598f98e4e392c3409f756d16
Brendan <brendan991@gmail.com>
2019-03-09 20:10:41 -0500

    working json object returned from get_recipe_links

--- Scraper/get_ingredients_request.js
+++ Scraper/get_ingredients_request.js
@@ -7,21 +7,33 @@ const base_url = 'http://foodnetwork.com/search/';
 let food_items_urls = [];
 let food_items = ['pizza', 'burger', 'sandwich', 'japanese-pancake'];
 
-get_ingredients(food_items);
+start_scraper(food_items);
 
-function get_ingredients(food_items)
+function start_scraper()
 {
+	recipe_links = get_recipe_links(food_items);
+	console.log(recipe_links);
+}
+
+function get_recipe_links(food_items)
+{
+	// Regex for filtering urls for recipes only
+	const regex = /^\/\/www.foodnetwork.com\/recipes\/.*/;
+	
 	// Create food item array to search
 	for (i = 0; i < food_items.length; i++)
 	{
 		food_items_urls.push(base_url.concat(food_items[i] + "-" ));
 	}
 
-	let real_urls = [];
+	let recipe_obj = {};
 
 	// Scrape site
 	for (i = 0; i < food_items_urls.length; i++)
 	{
+		let j = i;
+		console.log(i);
+		let real_urls = [];
 		request({
 			uri: food_items_urls[i],
 		}, function(error, response, body) {
@@ -29,22 +41,31 @@ function get_ingredients(food_items)
 
 			$("h3 > a").each(function() {
 				let link = $(this);
-				let text = link.text();
 				let href = link.attr("href");
 				real_urls.push(href);
-				
-				console.log(real_urls);
-				console.log(real_urls.length);
+				real_urls
 			});
+
+		let valid_urls = [];
+		// Filter urls to match only recipes
+		for (url of real_urls)
+		{
+			console.log(url);
+			if (regex.test(url))
+			{
+				valid_urls.push(url);
+			}
+			console.log(food_items + " --- something " + i);
+			//recipe_obj.food_items.push(real_urls);
+			recipe_obj[food_items[j]] = valid_urls;
+		}
+
+		console.log(recipe_obj);
+		return recipe_obj;
+		
 		});
 
-		//console.log(food_items_urls);
-		//console.log(food_items_urls[i]);
-		let pattern = /^href: '\/\/www.foodnetwork.com\/recipes\/.*' },/gi;
-		
-		/*while (match = pattern.exec(str))
-		{
-			alert(match[1]);
-		}*/
 	}
+	
 }
+
