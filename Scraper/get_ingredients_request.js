2d9b7866e1185242b48eb4b143d919a0b00d6ec4
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2019-03-10 07:29:21 -0400

    Revert "Sends requests at same time, but somehow slower"
    This reverts commit 7aa8e2e6914b8dd2cb6dcb89cffd0891e9ddd543.

--- Scraper/get_ingredients_request.js
+++ Scraper/get_ingredients_request.js
@@ -84,18 +84,14 @@ function get_recipe_links(food_items)
 	
 }
 
-var fulfilled = 0;
+
 async function findIngredientsAtLink(link, arr) {
 	return new Promise(resolve => {
-		request(link, async function (error, response, body) {
+		request(link, function (error, response, body) {
 			if(error){
-				fulfilled++;
 				console.error(error);
 				resolve(false);
-				return;
 			}
-			fulfilled++;
-			console.log("Network!");
 			let $ = cheerio.load(body);
 			let ingredients = $('.o-Ingredients__m-Body');
 			arr.push(ingredients.text().replace(/\s\s+/g, ' ').replace(/[0-9/:]/g, "").toLowerCase());
@@ -103,14 +99,13 @@ async function findIngredientsAtLink(link, arr) {
 		});
 	  });
 }
-var sentRequests = 0;
+
 async function findIngredientsForKey(source, arr){
 	const promises = [];
 	for(var link of source) {
-		sentRequests++;
 		promises.push(findIngredientsAtLink(link, arr)); 
 	}
-	return promises;
+	return Promise.all(promises);
 
 }
 
@@ -118,45 +113,114 @@ async function findIngredientsForKey(source, arr){
 // Get ingredients for each food items and return as JSON object
  async function get_ingredients(recipe_obj)
 {
-	var attempts = 0;
-	let promises = [];
+
 	var formattedObj = {};
 	for (var food in recipe_obj) {
 		if (recipe_obj.hasOwnProperty(food)) {
-			
 			formattedObj[food] = [];
-			 promises += findIngredientsForKey(recipe_obj[food], formattedObj[food]);
+			 await findIngredientsForKey(recipe_obj[food], formattedObj[food]);
 
 		}
 	}
-	console.log(promises);
-	return await Promise.all(promises)
-	.then((response) => {
-		let x = () =>{
-			attempts++;
-			//console.log(sentRequests + " " + fulfilled+ " at" + attempts);
-			return sentRequests == fulfilled || attempts > 100;
-		}
-		let y = () =>{
-			console.log("Callback");
-			//do stuff with response
-			endCallback(formattedObj)
-		}
-		let z = () => {
-			while(true){
-				if(x()){y(); return;};
-				setTimeout(z, 100);
-				return;
-			}
-		}
-		z();
-		
-	
-	
-	})
-	.catch((error) => {
-		console.error(error);
-	})
-	
 
+	endCallback(formattedObj);
+
+
+
+			/*formattedObj[food] = [];
+			let thisKey = food;
+			for( link of recipe_obj[thisKey]){
+				promises.push(rpromise(link)
+					.then(async function (htmlString) {
+						
+						
+						let $ = cheerio.load(htmlString);
+						let ingredients = $('.o-Ingredients__m-Body');
+						formattedObj[thisKey].push(ingredients.text().replace(/\s\s+/g, ' ').replace(/[0-9/:]/g, ""));
+						resolve(true);
+						// Process html...
+					})
+					.catch(async function (err) {
+						console.error(err);
+						resolve(false);
+					}));
+			}*/
+
+
+	/*console.log(recipe_obj);
+	let ingredient_obj = {};
+	let ingredient_list = [];
+
+	targetParsings = Object.keys(recipe_obj).length;
+	Object.keys(recipe_obj).forEach(async function(key) {
+	//	console.log(recipe_obj[key]);
+		
+		try{
+			await query_ingredients(recipe_obj[key], [], key); 
+		} catch(ex){
+			console.error(ex);
+			
+		}
+	});
+	return;*/
 }
+
+/*
+	let key_count_2 = Object.keys(recipe_obj).length;
+	let count_2 = 0;
+	console.log(key_count_2);
+
+	Object.keys(recipe_obj).forEach(function(key) {
+		console.log('key : ' + key + ', value : ' + recipe_obj[key])
+		count_2++;	
+
+		let ingredient_list = []; // hold stringified text of list of ingredients for food item
+
+		ingredient_obj[key] = [];
+
+		let key_count = recipe_obj[key].length;
+		let count = 0;
+		console.log(key_count);
+		// Get each food items ingredients
+		for (link of recipe_obj[key])
+		{
+			let ingredient_key = key;
+			
+			request({
+				url: link
+			}, function(err, res, body) {
+				if (err) 
+				{
+					return console.error(err);
+				}
+
+				let $ = cheerio.load(body);
+				let ingredients = $('.o-Ingredients__m-Body');
+				ingredient_list.push(ingredients.text().replace(/\s\s+/g, ' ').replace(/[0-9/:]/g, ""));
+				console.log("this is where it should add the ingredients");
+			
+				count++;
+				// Add to JSON object
+				ingredient_obj[ingredient_key].push(ingredient_list);
+				if (count == key_count && count_2 == key_count_2)
+				{
+					count = 9999;
+					console.log("@@@@@@@@@@@@@@@@@" + JSON.stringify(ingredient_obj[ingredient_key][0]));
+					v_callback();
+				}
+			});
+		}
+		console.log(ingredient_obj);
+	})
+	console.log(ingredient_obj);
+
+	Object.keys(ingredient_obj).forEach(function(key){
+		console.log('key : ' + key + ', value : ' +  ingredient_obj[key])
+	})
+	*/
+
+
+
+
+
+
